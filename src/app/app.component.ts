e3555b5d629e8f08531f63b69d699342f750709a
Enrique Delgado <enrique.delgado@live.com>
2024-09-07 21:13:20 -0600

    feature(Bootstrap): Initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BsDropdownModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
