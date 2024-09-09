78737d58ec1d9c8a2fe155cf2a27baa2369be0a4
Enrique Delgado <enrique.delgado@live.com>
2024-09-08 22:55:17 -0600

    feature(Responsive): Responsive for this site addedc

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from '../logo/logo.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [LogoComponent],
+  imports: [LogoComponent, RouterModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
-
+  currentYear = new Date().getFullYear();
 }
