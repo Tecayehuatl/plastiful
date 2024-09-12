29f90e4e7e35cfa0bdb6c92c29fef39267d207e5
Enrique Delgado Tecayehuatl <enrique.delgado@live.com>
2024-09-12 10:36:33 -0600

    feature(Responsive): Responsive improved and icons replaced by svg

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from '../logo/logo.component';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [LogoComponent, RouterModule],
+  imports: [LogoComponent, RouterModule, CommonModule, RouterModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
   currentYear = new Date().getFullYear();
+  isMenuShown = false;
+
+  constructor(private router: Router) {}
+
+  toggleMenu(): void {
+    this.isMenuShown = !this.isMenuShown;
+  }
+
+  navigateTo(url: string): void {
+    this.router.navigate([url]);
+    this.isMenuShown = false;
+  }
 }
